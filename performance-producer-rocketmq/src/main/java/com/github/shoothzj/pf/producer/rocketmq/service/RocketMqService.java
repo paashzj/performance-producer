8e74bf8afa2c8c0c5de6d54f49ae28aa5ffcc252
fu_turer <futurer@outlook.com>
2021-09-01 16:08:51 +0800

    RocketMq Service

--- performance-producer-rocketmq/src/main/java/com/github/shoothzj/pf/producer/rocketmq/service/RocketMqService.java
+++ performance-producer-rocketmq/src/main/java/com/github/shoothzj/pf/producer/rocketmq/service/RocketMqService.java
@@ -2,7 +2,9 @@ package com.github.shoothzj.pf.producer.rocketmq.service;
 
 import com.github.shoothzj.pf.producer.common.AbstractProduceThread;
 import com.github.shoothzj.pf.producer.common.config.ThreadConfig;
+import com.github.shoothzj.pf.producer.common.metrics.MetricBean;
 import com.github.shoothzj.pf.producer.common.metrics.MetricFactory;
+import com.github.shoothzj.pf.producer.common.module.OperationType;
 import com.github.shoothzj.pf.producer.common.util.RandomUtil;
 import com.github.shoothzj.pf.producer.rocketmq.config.RocketMqConfig;
 import lombok.extern.slf4j.Slf4j;
@@ -28,11 +30,15 @@ public class RocketMqService extends AbstractProduceThread {
 
     private final List<DefaultMQProducer> producers;
 
+    private final MetricBean metricBean;
+
     public RocketMqService(int index, MetricFactory metricFactory, ThreadConfig config, RocketMqConfig rocketMqConfig) {
         super(index, metricFactory, config);
         this.config = rocketMqConfig;
         this.producers = new ArrayList<>();
         this.random = new Random();
+        this.metricBean = newMetricBean(OperationType.PRODUCE);
+
     }
 
     @Override
@@ -51,6 +57,7 @@ public class RocketMqService extends AbstractProduceThread {
     @Override
     protected void send() {
 
+        long startTime = System.currentTimeMillis();
         int index = random.nextInt(config.producerNum);
         String topic = config.topic + "_" + index;
         DefaultMQProducer producer = producers.get(index);
@@ -59,11 +66,13 @@ public class RocketMqService extends AbstractProduceThread {
             producer.send(msg, new SendCallback() {
                 @Override
                 public void onSuccess(SendResult sendResult) {
+                    metricBean.success(System.currentTimeMillis() - startTime);
                     log.info("msg send to {} success. msgId {} queueOffset {}", topic, sendResult.getMsgId(), sendResult.getQueueOffset());
                 }
 
                 @Override
                 public void onException(Throwable e) {
+                    metricBean.fail(System.currentTimeMillis() - startTime);
                     log.error("{} send msg callback failed", topic, e);
                 }
             });
